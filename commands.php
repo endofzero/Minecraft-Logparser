8a30c86efd42268aeb9e361d798b94f7ffb1df6e
Ripley Jonez <endofzero@gmail.com>
2010-10-21 21:33:18 -0600

    Removed local echo

--- commands.php
+++ commands.php
@@ -20,14 +20,15 @@ This is a command line PHP script with one option.
   Usage:
   <?php echo $argv[0]; ?> <option>
 
-  <option> can be some word you would like
-  to print out. With the --help, -help, -h,
+	Current Commands:
+  inject : Will inject the logs into mysql based
+           on the settings in parser.settings
+
+  With the --help, -help, -h,
   or -? options, you can get this help.
 
 <?php
 } else {
-    echo $argv[1]."\n";
-
 	switch ($argv[1])
 	{
 	case "inject":
